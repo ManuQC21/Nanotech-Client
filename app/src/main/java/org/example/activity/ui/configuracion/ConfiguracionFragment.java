fed9da5a8702a95e5edaa195da6782abf0e215c5
ManuQC21 <mquispecueva1@upao.edu.pe>
2023-06-30 12:57:52 -0500

    Hasta ahí sin mimir :)

---
+++ app/src/main/java/org/example/activity/ui/configuracion/ConfiguracionFragment.java
@@ -0,0 +1,17 @@
+package org.example.activity.ui.configuracion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import org.example.R;
+public class ConfiguracionFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
+    }
+}
---
+++ app/src/main/java/org/example/activity/ui/home/InicioFragment.java
@@ -0,0 +1,19 @@
+package org.example.activity.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import org.example.R;
+
+public class InicioFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+}
